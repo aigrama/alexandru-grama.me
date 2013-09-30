458cfdf4ca40d03b7c017766ab4e8c005217576e
aigrama <gramaalexandruionut@gmail.com>
2013-09-30 08:30:26 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsAlexMain::Application.config.secret_token = '149d3729ad7e5886187b4f97d2c6c145832d2826cb80d40b2f6d37c5065e8abfc8d2b1b2f4d278a81a75743f58603c82c2181e1f20539c2c552c5c09c52a9bc3'
